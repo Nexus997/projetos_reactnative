9bdb3322026651dba1f110fe6b1185582d6192ff
Samuel Oliveira <samuel526lucas@gmail.com>
2026-09-03 20:53:38 -0300

    feat: cria estrutura inicial da tela de gastos

---
+++ 04.ControleGastos/src/app/index.tsx
@@ -0,0 +1,53 @@
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { 
+  Text, 
+  View, 
+  TextInput, 
+  TouchableOpacity,
+  StyleSheet } from 'react-native';
+
+
+export default function HomeScreen() {
+  return (
+
+
+
+<SafeAreaView style={styles.container}>
+  <Text>Controle de Gastos</Text>
+  <Text>Total gasto</Text>
+  <Text>R$ 0,00</Text>
+  <View>
+
+    <Text>Novo gasto</Text>
+    <TextInput placeholder="Descrição"/>
+    <TextInput placeholder="Valor" keyboardType="numeric"/>
+
+    <TouchableOpacity>
+      <Text>Adicionar</Text>
+    </TouchableOpacity>
+
+  </View>
+
+  <View>
+    <Text>Meus gastos</Text>
+
+    <View>      
+      <Text>Descrição</Text>
+      <Text>Valor</Text>
+    </View>
+
+  </View>
+
+
+
+
+</SafeAreaView>
+  );}
+
+
+  const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+});
